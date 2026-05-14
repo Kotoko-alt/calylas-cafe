56ec703c4e5e46fc3d8108fc0e0471c1ba038a7c
Kotoko-alt <bautistajimwel896@gmail.com>
2026-05-14 23:46:09 +0800

    connect render backend

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+      target: 'https://calylas-cafe.onrender.com',
         changeOrigin: true,
       }
     }
